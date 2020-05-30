4e860cba1f8280532af855777e66b5343b244d64
JUSEPHY JHONATHAN PAREDES RAMOS <jhonathanparedes@hotmail.es>
2020-05-31 00:35:15 +0200

    Añadido el orden ASC y DESC de la lista, muestra de petición orden usuario y retorno de éstos según escoga usuario

--- src/com/base/Circulos.java
+++ src/com/base/Circulos.java
@@ -12,5 +12,25 @@ public class Circulos {
         this.radio =radio;
     }
 
+    public double getRadio() {
+        return radio;
+    }
+
+    public void setRadio(double radio) {
+        this.radio = radio;
+    }
+
+    public double getPerímetro(double radio){
+        return Math.PI*radio*2;
+    }
+
+    public double getArea(double radio){
+        return Math.PI*(Math.pow(radio,2));
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s: %.2f %s: %.2f %s: %.2f","Radio",this.getRadio(),"Area",this.getArea(this.getRadio()),"Perímetro",this.getPerímetro(this.getRadio()));
+    }
 
 }
