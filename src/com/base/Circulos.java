a192749a7881ab58dd56944754eca304b8f63536
JUSEPHY JHONATHAN PAREDES RAMOS <jhonathanparedes@hotmail.es>
2020-05-30 23:23:52 +0200

    Añadir ArrayList y bucle de creación lista y radio aleatorio

---
+++ src/com/base/Circulos.java
@@ -0,0 +1,16 @@
+package com.base;
+
+public class Circulos {
+
+    public enum Atributo{
+        ASC,DESC;
+    }
+
+    private double radio;
+
+    public Circulos(double radio){
+        this.radio =radio;
+    }
+
+
+}
--- src/com/base/Main.java
+++ src/com/base/Main.java
@@ -1,8 +1,40 @@
 package com.base;
 
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class Main {
 
+    private static List<Circulos> circulos=new ArrayList<>();
+    private static final double MIN = 0.01;
+
     public static void main(String[] args) {
-        // write your code here
+
+        Main app=new Main();
+
+        int entradaDatos=Integer.parseInt(JOptionPane.showInputDialog("Desea crear una nueva lista?\n\tSi: 1 / No: 0 "));
+
+        boolean bucle=true;
+        while (bucle){
+            if (entradaDatos==0){
+                bucle=false;
+            }
+        }
+    }
+
+    public void inicio(){
+        boolean random=true;
+
+        while (random){
+            double radRandom = Math.random();
+            if (radRandom<MIN){
+                random=false;
+                continue;
+            }
+            var circ = new Circulos(radRandom);
+            circulos.add(circ);
+        }
     }
 }
